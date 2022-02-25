b1cf8c5144833966a5eecdff674fb8420bb386b6
MexpressoBean <kingk102@gmail.com>
2022-02-25 14:15:42 -0800

    built out api call and response parse methods

--- checkPass.rb
+++ checkPass.rb
@@ -1,3 +1,7 @@
+# Main author: Kevin Ramirez
+# Project: Cybersecurity center git project
+# Built for IT Practicum course at CSUDH
+
 # Password Security check script
 # This script can be used to check a given password against a large dataset of known bad 
 # passwords that have been recovered and included in past security breaches.
@@ -19,3 +23,54 @@ require "uri"
 require "net/http"
 require "digest/sha1"
 
+# METHODS
+# ------------------
+# create_hash method:
+# Input: raw password text to check
+# Return: SHA-1 hash of inputted password text
+def create_hash(password)
+    hash = Digest::SHA1.hexdigest password
+    hash.upcase
+end
+
+# check_password method:
+# Check password against dataset of known bad password
+# using HIBP API (https://haveibeenpwned.com/API/v3#PwnedPasswords)
+# Input: raw password text to check
+# Return: If inputted password is found in the bad password data, return 
+# found password object containing the hash and the number of occurences found in the data.
+# Match ouput looks like this -> {:full_hash=> "FFB4761CBA839470133BEE36AEB139F58D7DBAA9", :count=>"802"}
+def check_password(password)
+    hashed_password = create_hash(password)
+    hash_prefix = hashed_password.upcase[0,5]
+
+    # make request to HIBP API
+    url = URI("https://api.pwnedpasswords.com/range/#{hash_prefix}")
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+    request = Net::HTTP::Get.new(url)
+    response = https.request(request)
+    results = response.body.split("\n")
+
+    # parse response data
+    final_results = parse_response(results, hash_prefix)
+end
+
+# parse_response method:
+# Parse the raw response data from the api request into an array of ruby objects
+# Input: raw response data from the api request & the original password hash prefix
+# Return: Array of objects containing parsed response data (hash suffix and count)
+def parse_response(response_data, prefix)
+    results_ar = []
+    response_data.each do |r|
+        hash_suffix = r.split(":")[0] # isolate suffix
+        count = r.split(":")[1].chomp # isolate count
+        results_ar << {:full_hash => "#{prefix}#{hash_suffix}", :count => count}
+    end
+    results_ar
+end
+
+
+# PROGRAM START
+
+puts check_password("kevin")
